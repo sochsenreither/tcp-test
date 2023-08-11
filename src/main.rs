27ff3ca2db826128f9a412ff0519ba88f1dfae17
Simon Ochsenreither <s8siochs@stud.uni-saarland.de>
2023-08-11 09:34:11 +0200

    updated networking

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,21 @@ mod node;
 
 #[tokio::main]
 async fn main() {
-    let nodes = 8;
+    let n = 5;
     let runtime = 15;
 
-    // Create 4 local ip addresses with different ports.
-    let addresses = (0..nodes)
+    // Create n local ip addresses with different ports.
+    let addresses = (0..n)
         .map(|x| format!("127.0.0.1:123{}", x).parse::<SocketAddr>().unwrap())
         .collect::<Vec<_>>();
 
-    // Spawn 4 nodes.
-    for i in 0..nodes {
+    // Spawn n nodes.
+    for i in 0..n {
         let addresses = addresses.clone();
         tokio::spawn(async move {
             node::Node::new(i, addresses).await;
         });
     }
 
-    // Wait 15 seconds before terminating.
     sleep(Duration::from_secs(runtime)).await;
 }
--- src/node.rs
+++ src/node.rs
@@ -3,24 +3,29 @@ use std::net::SocketAddr;
 use tokio::sync::mpsc::channel;
 use tokio::time::{sleep, Duration};
 
-use crate::{
-    core::Core,
-    network::{MessageReceiver, SimpleSender},
-};
+use crate::{core::Core, network::*};
 
 pub struct Node;
 
 impl Node {
     pub async fn new(id: usize, nodes: Vec<SocketAddr>) {
-        // Create a channel for the message receiver. The receiver receives data from incoming
-        // tcp connections and puts this data into the channel. The data is retreives via the rx
-        // channel.
-        let (tx, rx) = channel(1_000);
-        MessageReceiver::spawn(nodes[id], tx);
-        let sender = SimpleSender::new();
+        // Create channels for the networking.
+        let (tx_rec, rx_rec) = channel(10_000);
+        let (tx_send, rx_send) = channel(10_000);
+
+        // Create a network receiver and sender.
+        let network_receiver = NetworkReceiver::new(nodes[id], tx_rec);
+        let mut network_sender = NetworkSender::new(rx_send);
+
+        tokio::spawn(async move {
+            network_receiver.run().await;
+        });
+        tokio::spawn(async move {
+            network_sender.run().await;
+        });
 
         sleep(Duration::from_millis(50)).await;
 
-        Core::spawn(id, nodes, sender, rx);
+        Core::spawn(id, nodes[id], nodes, tx_send, rx_rec);
     }
 }
